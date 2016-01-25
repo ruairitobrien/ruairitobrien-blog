5bb8329dd9c7986b1a855a568981c97386adbd7e
Ruairi O Brien <ruairitobrien@gmail.com>
2016-01-25 21:19:26 +0000

    added boilerplate blog code

---
+++ docpad.js
@@ -0,0 +1,141 @@
+var docpadConfig, moment;
+
+moment = require('moment');
+
+docpadConfig = {
+    templateData: {
+        site: {
+            title: 'Realgorithm',
+            tagline: 'What\'s a computer?',
+            description: 'I possibly might say something useful every once and awhile.',
+            logo: '/img/logo.png',
+            url: '',
+            cover: '/img/cover.jpg',
+            navigation: [
+                {
+                    name: 'Home',
+                    href: '/',
+                    section: 'home'
+                }, {
+                    name: 'About',
+                    href: '/about.html',
+                    section: 'about'
+                }, {
+                    name: 'Fiction',
+                    href: '/tags/fiction.html',
+                    section: 'tag-fiction'
+                }, {
+                    name: 'Test Posts',
+                    href: '/tags/test.html',
+                    section: 'tag-test'
+                }
+            ]
+        },
+        author: {
+            name: 'John U. Doe',
+            img: '',
+            url: '/',
+            location: 'Nowhere, IL',
+            bio: 'I do stuff and things'
+        },
+        getPreparedTitle: function () {
+            if (this.document.title) {
+                return this.document.title + " | " + this.site.title;
+            } else {
+                return this.site.title;
+            }
+        },
+        getDescription: function () {
+            if (this.document.description) {
+                return this.document.description + " | " + this.site.description;
+            } else {
+                return this.site.description;
+            }
+        },
+        bodyClass: function () {
+            if (this.document.isPost) {
+                return "post-template";
+            } else {
+                return "home-template";
+            }
+        },
+        masthead: function (d) {
+            d = d || this.document;
+            if (d.cover) {
+                return d.cover;
+            } else {
+                return this.site.cover;
+            }
+        },
+        isCurrent: function (l) {
+            if (this.document.section === l.section) {
+                return ' nav-current';
+            } else if (this.document.url === l.href) {
+                return ' nav-current';
+            } else {
+                return '';
+            }
+        },
+        excerpt: function (p, w) {
+            w = w || 26;
+            if (p.excerpt) {
+                return p.excerpt;
+            } else {
+                return p.content.replace(/<%.+%>/gi, '').split(' ').slice(0, w).join(' ');
+            }
+        },
+        encode: function (s) {
+            return encodeURIComponent(s);
+        },
+        slug: function (s) {
+            return s.toLowerCase().replace(' ', '-');
+        },
+        currentYear: function () {
+            return new Date().getFullYear();
+        },
+        time: function (ts, format) {
+            format = format || 'MMMM DO, YYYY';
+            ts = new Date(ts) || new Date();
+            return moment(ts).format(format);
+        }
+    },
+    collections: {
+        posts: function () {
+            return this.getCollection("html").findAllLive({
+                active: true,
+                isPost: true,
+                isPagedAuto: {
+                    $ne: true
+                }
+            }, {
+                    postDate: -1
+                }).on("add", function (model) {
+                    return model.setMetaDefaults({
+                        layout: "post"
+                    });
+                });
+        }
+    },
+    plugins: {
+        tags: {
+            extension: '.html',
+            injectDocumentHelper: function (doc) {
+                return doc.setMeta({
+                    layout: 'tag'
+                });
+            }
+        },
+        rss: {
+            "default": {
+                collection: 'posts',
+                url: '/rss.xml'
+            }
+        },
+        ghpages: {
+            deployRemote: 'target',
+            deployBranch: 'master'
+        }
+    }
+};
+
+module.exports = docpadConfig;
---
+++ src/render/about.html.md
@@ -0,0 +1,10 @@
+---
+title: 'About'
+layout: 'page'
+---
+
+My name is Ruairi O'Brien.
+
+I'm a programmer workign for a groups called PS APPs in EMC.
+ 
+I used to do other stuff but I've forgotten all that now.
